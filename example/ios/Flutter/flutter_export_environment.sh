08ff5d7af5033f63055f97f31f773ec6b2eb2b77
Guðmundur Stefán Þorvaldsson <gummis@gmail.com>
2019-12-30 13:03:46 +0000

    chore: build things

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gummis/frameworks/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gummis/code/lotta_forks/audioplayer/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gummis/frameworks/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=2.0.0"
